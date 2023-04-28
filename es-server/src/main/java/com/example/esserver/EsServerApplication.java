141b0b696cbdf37148ce117c44aee06642db0e48
shyPole <1014325296@qq.com>
2023-04-28 12:31:22 +0800

    第三次提交

--- es-server/src/main/java/com/example/esserver/EsServerApplication.java
+++ es-server/src/main/java/com/example/esserver/EsServerApplication.java
@@ -1,13 +1,41 @@
 package com.example.esserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EsServerApplication {
 
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EsServerApplication.class, args);
+    }
+
+    @RestController
+    public class TestController {
+
+        private final RestTemplate restTemplate;
+
+        @Autowired
+        public TestController(RestTemplate restTemplate) {this.restTemplate = restTemplate;}
+
+        @GetMapping("/echo/{str}")
+        public String echo(@PathVariable String str) {
+            return restTemplate.getForObject("http://nacos-provider/echo/" + str, String.class);
+        }
+
     }
 
 }
--- nacos-server/src/main/java/com/example/nacosserver/NacosServerApplication.java
+++ nacos-server/src/main/java/com/example/nacosserver/NacosServerApplication.java
@@ -2,12 +2,25 @@ package com.example.nacosserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NacosServerApplication.class, args);
     }
 
+    @RestController
+    class EchoController {
+
+        @GetMapping("/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
 }
