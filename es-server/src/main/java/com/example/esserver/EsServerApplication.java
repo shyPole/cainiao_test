978a1a84ebc732a67c76decd37b009f4c68a9048
shyPole <1014325296@qq.com>
2023-04-29 13:02:49 +0800

    es架包添加

--- es-server/src/main/java/com/example/esserver/EsServerApplication.java
+++ es-server/src/main/java/com/example/esserver/EsServerApplication.java
@@ -3,14 +3,18 @@ package com.example.esserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories
+@EnableDiscoveryClient
 public class EsServerApplication {
 
     @LoadBalanced
